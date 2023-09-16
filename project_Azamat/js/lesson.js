357fc32fbc04bcf232507d454f261986ab459c8b
mqqqq11 <141828182+mqqqq11@users.noreply.github.com>
2023-09-16 13:18:26 +0600

    first'

--- project_Azamat/js/lesson.js
+++ project_Azamat/js/lesson.js
@@ -15,3 +15,51 @@ phoneButton.onclick = () => {
     }
 }
 
+//TAB SLIDER PL
+const tabContentBlocks = document.querySelectorAll('.tab_content_block')
+const tabsParentBlock = document.querySelector('.tab_content_items')
+const tabsBlocks = document.querySelectorAll('.tab_content_item')
+
+const hideTabContent = () => {
+    tabContentBlocks.forEach((tabContentBlock) => {
+        tabContentBlock.style.display = 'none'
+    })
+    tabsBlocks.forEach((tabBlock) => {
+        tabBlock.classList.remove ('tab_content_item_active')
+    })
+}
+
+const showTabContent = (indexElement = 0) => {
+    tabContentBlocks[indexElement].style.display = 'block'
+    tabsBlocks[indexElement].classList.add('tab_content_item_active')
+}
+
+hideTabContent()
+showTabContent()
+
+
+tabsParentBlock.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabsBlocks.forEach((tabBlock, tabIndex) => {
+            if (event.target === tabBlock ) {
+                hideTabContent()
+                showTabContent(tabIndex)
+            }
+        })
+    }
+}
+
+let index = 0
+
+const autoShow = (indexElement = 0) => {
+    hideTabContent()
+    showTabContent(indexElement)
+
+    setInterval(() => {
+        hideTabContent()
+        showTabContent(index)
+        index = (index + 1 >= tabsBlocks.length) ? 0 : index + 1;
+    },3000) 
+}
+autoShow()
+
